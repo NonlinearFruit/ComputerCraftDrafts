495d24db4399b15865cb5dadbf0c7b682572e0ff
NonlinearFruit <1123benji5813@gmail.com>
2021-03-24 11:16:01 -0500

    Make busted vanilla lua runnable

--- busted.lua
+++ busted.lua
@@ -1,12 +1,25 @@
-programs = shell.programs();
-
 specs = {}
-for _, program in pairs(programs) do
+function add_spec(program)
   if string.match(program, "_spec") then
     module = require(program);
     for func, _ in pairs(module) do
       table.insert(specs, {func = module[func], name = program..":"..func});
     end
+  end
+end
+
+if shell == nil then
+  term, colors = {}, {}
+  term.write = io.write
+  term.setTextColor = function() end
+  programs = io.popen("ls -1 | sed -e 's/\\..*$//'"):lines();
+  for program in programs do
+    add_spec(program)
+  end
+else
+  programs = shell.programs();
+  for _, program in pairs(programs) do
+    add_spec(program)
   end
 end
 
